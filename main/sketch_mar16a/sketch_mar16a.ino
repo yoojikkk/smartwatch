8bf102ee7235c530c996f34aab7c6de68a6960fb
John Vince Papa <jvrpapa05@gmail.com>
2024-03-16 17:06:38 +0800

    Send SMS when BPM is not valid continuously.

--- main/sketch_mar16a/sketch_mar16a.ino
+++ main/sketch_mar16a/sketch_mar16a.ino
@@ -22,6 +22,10 @@ SoftwareSerial mySerial(3, 2); //SIM800L Tx & Rx is connected to Arduino #3 & #2
 //////////////////////////////////////////////////////////////////////////////////////////////////
 
 
+#define BPM_NOT_VALID 10
+#define BPM_SEND_SMS  100
+
+
 void setup()
 {
   Serial.begin(9600);
@@ -59,10 +63,19 @@ void setup()
 
 void loop()
 {
-  //send_SMS(String(get_BPM()));
-  //spo2_sensor();
 
-  //Serial.println(get_BPM());
+  if (finger_present() == true){
+
+    if (get_BPM() > BPM_NOT_VALID){
+      Serial.println(get_BPM());
+      send_SMS(String(get_BPM() + '\n'));
+    }
+
+  }else{
+    Serial.println("Place your finger.");
+    delay(1000);
+  }
+
 }
 
 void send_SMS(String message){
@@ -147,7 +160,34 @@ float get_BPM(){
 }
 
 boolean finger_present(){
-  
+  long irValue = particleSensor.getIR();
+
+  if (checkForBeat(irValue) == true)
+  {
+    //We sensed a beat!
+    long delta = millis() - lastBeat;
+    lastBeat = millis();
+
+    beatsPerMinute = 60 / (delta / 1000.0);
+
+    if (beatsPerMinute < 255 && beatsPerMinute > 20)
+    {
+      rates[rateSpot++] = (byte)beatsPerMinute; //Store this reading in the array
+      rateSpot %= RATE_SIZE; //Wrap variable
+
+      //Take average of readings
+      beatAvg = 0;
+      for (byte x = 0 ; x < RATE_SIZE ; x++)
+        beatAvg += rates[x];
+      beatAvg /= RATE_SIZE;
+    }
+  }
+
+  if (irValue < 50000){
+    return false;
+  } else{
+    return true;
+  }
 }
 
 
